f466a18c9c6d6fb3b1c4cf705a4a631be788892a
Konstantin-Pavlov <konstpavlov87@gmail.com>
2024-10-15 11:51:20 +0600

    fix adding element to the map

--- src/main/java/uno/soft/serviece/grouper/LinesEffectiveGrouper.java
+++ src/main/java/uno/soft/serviece/grouper/LinesEffectiveGrouper.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -17,7 +19,8 @@ public class LinesEffectiveGrouper {
                 "\"111\";\"123\";\"222\"",
                 "\"200\";\"123\";\"100\"",
                 "\"300\";;\"100\"",
-                "\"400\";\"456\";\"300\""
+                "\"400\";\"456\";\"300\"",
+                "\"300\";\"556\";\"101\""
         );
 
         groupLines(lines);
@@ -40,23 +43,42 @@ public class LinesEffectiveGrouper {
                     continue;
                 }
 
-                UUID uuid = UUID.randomUUID();
-                elementsMap.computeIfAbsent(element, k -> new HashSet<>()).add(new Element(uuid, line, lineIndex, columnIndex));
-                // todo - fix; we add to LineTracker every element UUID in line, it's wrong
-                if(lineTrackerMap.containsKey(line)) {
-                    lineTrackerMap.get(line).getElementUuidsSetLineBelongsTo().add(uuid);
+
+                //todo - element represents also its column index in elementsMap
+
+                // make sure lines share one element have the same uuid
+                if (!elementsMap.containsKey(element)) {
+                    UUID uuid = UUID.randomUUID();
+                    HashSet<Element> set = new HashSet<>();
+                    set.add(new Element(uuid, line, lineIndex, columnIndex));
+                    elementsMap.put(element, set);
                 } else {
-                    LineTracker lineTracker = new LineTracker(line);
-                    lineTracker.getElementUuidsSetLineBelongsTo().add(uuid);
-                    lineTrackerMap.put(line, lineTracker);
+                    Optional<Element> first = elementsMap.get(element).stream().findFirst();
+                    if (first.isPresent()) {
+                        UUID uuid = first.get().getUuid();
+                        Objects.requireNonNull(elementsMap.get(element)).add(new Element(uuid, line, lineIndex, columnIndex));
+                    } else {
+                        throw new IllegalArgumentException("Element " + element + " not found");
+                    }
                 }
+
+//                elementsMap.computeIfAbsent(element, k -> new HashSet<>()).add(new Element(uuid, line, lineIndex, columnIndex));
+                // todo - after: fix; we add to LineTracker every element UUID in line, it's wrong
+//                if (lineTrackerMap.containsKey(line)) {
+//                    lineTrackerMap.get(line).getElementUuidsSetLineBelongsTo().add(uuid);
+//                } else {
+//                    LineTracker lineTracker = new LineTracker(line);
+//                    lineTracker.getElementUuidsSetLineBelongsTo().add(uuid);
+//                    lineTrackerMap.put(line, lineTracker);
+//                }
 
             }
             lineIndex++;
         }
 
         for (Map.Entry<String, Set<Element>> entry : elementsMap.entrySet()) {
-            if (entry.getValue().size() > 1) {}
+            if (entry.getValue().size() > 1) {
+            }
         }
 
         return groups;
